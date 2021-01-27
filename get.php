073c371af58044803681ee22e7dc70395fa9dd9b
suneel3214 <suneelchandel935@gmail.com>
2021-01-27 15:23:36 +0530

    new project lighting product

---
+++ get.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form>
+	username:<br>
+	<input type="text" name="" placeholder="username">
+<br><br>
+	password:<br>
+	<input type="password" name="" placeholder="password">
+	<br><br>
+	<button onclick="">submit</button>
+</form>
+</body>
+</html>
